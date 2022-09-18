e37590c93af2860983cbdaaf60d54d61377e025d
citnes <felipedelta0@gmail.com>
2022-09-18 00:10:05 -0300

    usando jpql

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,13 +1,19 @@
 package br.com.alura.spring.data.repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 
     List<Funcionario> findByNome(String nome);
+
+    @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.dataContratacao = :data AND f.salario >= :salario")
+    List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, BigDecimal salario, LocalDate data);
 }
--- src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -3,12 +3,17 @@ package br.com.alura.spring.data.service;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 @Service
 public class RelatoriosService {
 
     private final FuncionarioRepository funcionarioRepository;
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public RelatoriosService(FuncionarioRepository funcionarioRepository) {
         this.funcionarioRepository = funcionarioRepository;
@@ -18,6 +23,7 @@ public class RelatoriosService {
         while (true) {
             System.out.println("\n[RELATORIO] Qual ação deseja executar?");
             System.out.println("1 - Busca Funcionario por Nome");
+            System.out.println("2 - Busca Funcionario por Nome, Data de Contratação e Salário");
             System.out.println("0 - Sair");
 
             int action = sc.nextInt();
@@ -25,6 +31,10 @@ public class RelatoriosService {
             switch (action) {
                 case 1:
                     buscarFuncionarioPorNome(sc);
+                    break;
+
+                case 2:
+                    buscarPorNomeSalarioMaiorDataContratacao(sc);
                     break;
 
                 default:
@@ -37,6 +47,20 @@ public class RelatoriosService {
         System.out.println("Informe o nome a buscar: ");
         String nome = sc.next();
         funcionarioRepository.findByNome(nome)
+                .forEach(System.out::println);
+    }
+
+    private void buscarPorNomeSalarioMaiorDataContratacao(Scanner sc) {
+        System.out.println("Informe o nome a buscar: ");
+        String nome = sc.next();
+
+        System.out.println("Informe a data de contratação a buscar: ");
+        LocalDate data = LocalDate.parse(sc.next(), formatter);
+
+        System.out.println("Informe o salário a buscar: ");
+        BigDecimal salario = sc.nextBigDecimal();
+
+        funcionarioRepository.findNomeSalarioMaiorDataContratacao(nome, salario, data)
                 .forEach(System.out::println);
     }
 }
